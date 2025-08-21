8968c5709371583a3eb4f824a70457462ffe0e30
Souritra Samanta <souritrasamanta@gmail.com>
2025-08-21 18:17:20 +0800

    Update main.py
    Annotated.

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import sys
 import os
 from cli_interface import SimplifiedHDBCalculatorCLI
 
+# This is the entry-point of the entire project
 def main():
-    try:
+    try: # Checks if packages are available
         import pandas, sklearn, numpy
         print("[SUCCESS] All packages available")
     except ImportError as e:
         print("[ERROR] Missing package: {}".format(e)) # souritra (watermark)
         return
     
-    if not os.path.exists('sample_data.csv'):
+    if not os.path.exists('sample_data.csv'): # Checks if dataset is available
         print("[ERROR] Could you download sample_data.csv first bro...")
         return
     
     calculator = SimplifiedHDBCalculatorCLI()
     calculator.run()
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Runs code
     main()
 
 # SOURITRA SAMANTA (3C)
